8b75a502557ba1dd84e20432b0a60af88be7fb79
tcbaoit <90180600+tcbaoit@users.noreply.github.com>
2025-10-16 15:47:08 +0700

    change abstractbase to enum for dynamic class

--- src/Luban.Rust/TypeVisitors/RustJsonUnderlyingDeserializeVisitor.cs
+++ src/Luban.Rust/TypeVisitors/RustJsonUnderlyingDeserializeVisitor.cs
@@ -62,9 +62,7 @@ public class RustJsonUnderlyingDeserializeVisitor : ITypeFuncVisitor<string, str
 
     public string Accept(TBean type, string json, string field, int depth)
     {
-        return type.DefBean.IsAbstractType
-            ? $"{RustCommonTemplateExtension.FullName(type.DefBean)}::new(&{json})?"
-            : $"{type.Apply(RustDeclaringTypeNameVisitor.Ins)}::new(&{json})?";
+        return $"{type.Apply(RustDeclaringTypeNameVisitor.Ins)}::new(&{json})?";
     }
 
     public string Accept(TArray type, string json, string field, int depth)
